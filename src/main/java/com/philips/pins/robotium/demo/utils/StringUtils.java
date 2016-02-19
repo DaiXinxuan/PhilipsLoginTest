3e10e3822d101081df927a869f7f5b5d6c3bafbb
daixinxuan <499587417@qq.com>
2016-02-19 15:58:47 +0800

    Adding constant to remove warning

---
+++ src/main/java/com/philips/pins/robotium/demo/utils/StringUtils.java
@@ -0,0 +1,18 @@
+package com.philips.pins.robotium.demo.utils;
+
+/**
+ * Created by dxx on 2016/2/19.
+ */
+public class StringUtils {
+    public final static String USERNAME = "username";
+    public final static String PROFILE = "profile";
+    public final static String COUNT = "count";
+    public final static String EMPTY_TOAST_USERNAME = "Username can not be empty";
+    public final static String EMPTY_TOAST_PASSWORD = "Password can not be empty";
+    public final static String NONEXISTENT_USER = "The username does not exist";
+    public final static String LOGIN_SUCCESS = "Successful login";
+    public final static String LOGIN_FAIL = "Login failed";
+    public final static String USERNAME_REUSED = "Username has already been registered";
+    public final static String REGIST_SUCCESS = "Regist successfully";
+    public static final String PASSWORD = "password";
+}
